9099bebd007ed68f88ad99e618276d5faab66b33
LGU <l.guitton@c2is.fr>
2013-02-08 12:38:58 +0100

    Ajout de css/screen.css vide pour structure OneTea

--- html/includes/inc_footer.php
+++ html/includes/inc_footer.php
@@ -1,5 +1,5 @@
 	<script src="../js/front.js"></script>
-    
+
 	<!-- Prompt IE 6 users to install Chrome Frame -->
 	<!--[if lt IE 7 ]>
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
