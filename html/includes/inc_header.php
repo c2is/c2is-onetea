ea62e40f9fa5b61cc956b11cd9ee91a55815b78e
Kaynshin <kaynshin.web@gmail.com>
2013-09-13 10:50:53 +0200

    [CHROMEFRAME] Suppression du lien vers chromframe {status:done} (#DJO).

--- html/includes/inc_header.php
+++ html/includes/inc_header.php
@@ -31,4 +31,4 @@
     </script> -->
 </head>
 <body>
-    <!--[if lte IE 7]><p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p><![endif]-->
+    <!--[if lte IE 7]><p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
